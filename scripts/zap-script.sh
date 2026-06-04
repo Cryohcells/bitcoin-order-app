756af2e3b65b32359520cd14ad609ec2be8861dc
Cryohcells <1547216322@qq.com>
2026-06-04 17:52:01 +0800

    Update zap-script.sh

--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 docker pull zaproxy/zap-stable
-docker run --rm -v "$(pwd):/zap/wrk/:rw" -t zaproxy/zap-stable \
-  zap-baseline.py -t "https://cryohcells.github.io/bitcoin-order-app/" \
+
+docker run --rm -v "${GITHUB_WORKSPACE}:/zap/wrk/:rw" -t zaproxy/zap-stable \
+  zap-baseline.py -t "https://kenken64.github.io/bitcoin-order-app/" \
   -r zap_baseline_report.html -l PASS
 
-echo $? > /dev/null
+if [ ! -f "${GITHUB_WORKSPACE}/zap_baseline_report.html" ]; then
+  echo "ZAP report was not generated."
+  exit 1
+fi
+
+exit 0
